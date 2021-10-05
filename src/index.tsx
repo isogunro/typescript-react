01fe22e62a1b6ce082a456fa7f182fe2d54168d3
Isaac Sogunro <isogunro@gmail.com>
2021-10-05 18:12:52 -0400

    removed reference to components

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,12 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Plus from './Plus';
-import Minus from './Minus';
-import  Divide from './Divide';
-import  Multiply from './Multiply';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Plus />
-    <Minus />
-    <Divide />
-    <Multiply />
   </React.StrictMode>,
   document.getElementById('root')
 );
